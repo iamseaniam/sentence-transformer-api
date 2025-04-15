976cd51f09180eca945f1332c1e3af7e0fa58923
iamseaniam <sean.alexander613@gmail.com>
2025-04-15 17:40:50 -0500

    moved more files, again sorry ace. stole them

---
+++ fetch_recommendations_V2.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python3
+
+# Going into this with a little bit more of a clear head.
+# I still don't know what I'm doing, but at least a little bit less so
+# Or more so? Not sure how that works out really.
+
+# Anyway, this script is what calls for the different bits and bobs
+# And returns them
+# It's got the models that will hold the data to be returned
+# As well as the SQL Query to gather the posts based on post IDs.
+
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+import psycopg2
+
+import os
+from typing import Optional, List
+from datetime import datetime
+
+# import cosine_similarity as cos_sim
+
+app = FastAPI()
+
+# Connecting to database
+def get_db_connection():
+    conn = psycopg2.connect(os.getenv("POSTGRES_URL"))
+    return conn
+
+# All of these will be changed when I see what the API calls look like
+# These are for JSONifying the data to be returned in an API call later.
+class UserProfile(BaseModel):
+    user_id: int
+    name: str
+    avatar: str
+    # avatar data type will more than likely be changed
+
+class PostRecommendation(BaseModel):
+    post_id: int
+    text: str
+    goods: str
+    image: str #Once again this will more than likely be changed
+    created_at: datetime
+    user: UserProfile
+
+# This is taking the list of post IDs gathered in get_recommendations
+# And running them through a SQL query to get all the post info
+# Then JSONifying it to return as an API call
+def aggrigate_and_JSONify(post_ids, conn):
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        -- This entire query will need to be adjusted
+        SELECT p.id, p.text, p.timestamp, p.image,
+            goods.name as goods, u.name, u.image
+        FROM posts p
+        JOIN goods on goods.id = p.good_id
+        JOIN users u ON p.user_id = u.id
+        WHERE p.id = ANY(%s::uuid[]);
+    """, (post_ids,))
+
+    post_details = cursor.fetchall()
+
+    # Not sure if there needs to be a change in how row[0] works
+    # Just because it feels weird that it's not got a name.
+
+    populated_post_recs = []
+    for row in post_details:
+        populated_post_recs.append({
+                "post_id": row[0],
+                "text": row[1],
+                "timestamp": row[2],
+                "image": row[3],
+                "goods": row[4],
+                "user": {
+                    "name": row[5],
+                    "avatar": row[6]
+                },
+        })
+
+    return populated_post_recs
+
+
+
+# @app.get("/recommendations/{user_id}", response_model=List[PostRecommendation])
+# I genuinely do NOT understand this.
+# How have I been so stupid for this long?
+# Or rather, how have I been over thinking this for this long?
+# Need me some brain smoother.
+def get_recommendations(user_id):
+    conn = get_db_connection()
+    # cursor = conn.cursor()
+    # Need cursor if not use cursor here?
+    # Does not appear so.
+
+    post_id_recs = []
+
+    cos_sim_recs = cos_sim.recommend_cosine_sim(user_id, conn)
+
+    post_id_recs.append(cos_sim_recs)
+
+    # Here's where we would call Sean's other functions
+    # and append them to a list
+
+    populated_post_recs = aggrigate_and_JSONify(post_id_recs, conn)
+
+    conn.close()
+
+    return populated_post_recs
+
+def test_jsonify():
+    # This is designed to grab post info for the jsonify function.
+    # I already know the jsonify function will need to change
+
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    test_posts = []
+
+    cursor.execute("""
+        SELECT id FROM posts;
+                   """)
+    
+    test_posts = cursor.fetchall()
+
+    populated_test_posts = aggrigate_and_JSONify(test_posts, conn)
+
+    conn.close()
+
+    return populated_test_posts
---
+++ generate_post_embeddings.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python3
+
+# I have no idea what a postgres implimentation looks like
+# So I don't know if we need a shebang
+# Anyway here's some imports
+
+# import psycopg2.pool
+from sentence_transformers import SentenceTransformer
+# import json
+
+# I imagine that since this'll be running on cody's machine
+# I won't need to have this downloaded.
+# Jury's out though
+#   I am in fact downloading this as I am testing it on my own machine
+#   Also, past Ace, it wasn't that big of a deal.
+#   Don't be a baby next time
+import psycopg2
+import re
+import numpy as np
+# We'll also need to pull the env variable
+# So I'm importing OS for now
+import os
+from dotenv.main import load_dotenv
+from fastapi import FastAPI
+
+app = FastAPI()
+
+model = SentenceTransformer('paraphrase-albert-small-v2')
+load_dotenv()
+
+
+def pull_tags(text):
+    return re.findall(r'#\w+', text)
+
+
+def clean_text(text):
+    return re.sub(r'#\w+', '', text)
+
+
+def camel_case_split(str):
+    return re.sub(r'([a-z])([A-Z])',
+                  r'\1 \2', str)
+
+
+def preprocess_tags(tags):
+    if isinstance(tags, str):
+        tags = camel_case_split(tags)
+        return [tag.strip() for tag in tags.split(',') if tag.strip()]
+    elif isinstance(tags, list):
+        return [camel_case_split(tag) for tag in tags]
+    return []
+
+
+def generate_weighted_embeddings(post):
+    print(f"generating embeddings for post id {post['id']}")
+
+    tags = pull_tags(post['text'])
+    tags = preprocess_tags(tags)
+    print(f"tags pulled: {tags}")
+
+    text = clean_text(post['text'])
+    print(f"text pulled: {text}")
+
+    goods = post['goods']
+    print(f"goods pulled: {goods}")
+
+    tag_embedding = model.encode(" ".join(tags)) if tags else np.zeros(384)
+    goods_embedding = model.encode(goods) * 2
+    # *2 so we put more weight on the goods that the user wants
+    # This can be changed later, hyperparameter tuning is vibes based
+    text_embedding = model.encode(text) if text else np.zeros(384)
+
+    combined = np.concatenate([
+        tag_embedding,
+        goods_embedding,
+        text_embedding * 0.3
+        # more vibes based hyperparameter tuning
+    ])
+
+    return combined
+
+
+@app.get("/api/py/embed")
+# Do I need these if the API calls happen somewhere else???
+# Is it that easy???????
+def update_post_embeddings():
+    # This will need some error handling but for now I think it's ok
+    post_query = """SELECT posts.id, text, goods.name as goods
+                    FROM posts JOIN goods on goods.id = posts.good_id
+                    WHERE embedding IS NULL;"""
+    # limit might be needed but unsure how will loop it just yet
+    # Will limit things when able
+
+    # stuff for connecting to the database here
+
+    conn = psycopg2.connect(os.getenv("DATABASE_URL"))
+
+    print("Data Connection Info:")
+    print(os.getenv("DATABASE_URL"))
+
+    cursor = conn.cursor()
+
+    cursor.execute(post_query)
+    posts = cursor.fetchall()
+
+    if not posts:
+        print("All posts have embeddings")
+        return
+
+    update_count = 0
+
+    for post in posts:
+        print(f"Data Type: {type(post)}")
+        print(f"Post info: {post}")
+        post = list(post)
+        print(f"List Data Type: {type(post)}")
+        print(f"List Post info: {post}")
+
+        post_dict = {
+            "id": post[0],
+            "text": post[1],
+            "goods": post[2]
+        }
+
+        embedding = generate_weighted_embeddings(post_dict)
+
+        # Maybe do some truncating here
+        # Some PCA, you know?
+        # SVD is what David says
+
+        cursor.execute("""
+            UPDATE posts
+            SET embedding = %s
+            WHERE id = %s;
+        """, (embedding.tolist(), post_dict["id"]))
+        # Not sure if it needs to be tolist()
+        # Since the datatype is a vector.
+
+        update_count += 1
+
+    conn.commit()
+    conn.close()
+    print(f"{update_count} posts updated succesfully.")
+
+
+if __name__ == "__main__":
+    update_post_embeddings()
